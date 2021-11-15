f26de92928faa3d9da513869930a00bc403b8fec
pipe1990 <afca100@hotmail.com>
2021-11-15 15:29:35 -0500

    Initial commit

---
+++ src/main/java/edu/unbosque/JPATutorial/jpa/repositories/AuthorRepository.java
@@ -0,0 +1,18 @@
+package edu.unbosque.JPATutorial.jpa.repositories;
+
+import edu.unbosque.JPATutorial.jpa.entities.Author;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface AuthorRepository {
+
+    Optional<Author> findById(Integer id);
+
+    List<Author> findAll();
+
+    Optional<Author> save(Author author);
+
+    void deleteById(Integer id);
+
+}
---
+++ src/main/java/edu/unbosque/JPATutorial/jpa/repositories/AuthorRepositoryImpl.java
@@ -0,0 +1,67 @@
+package edu.unbosque.JPATutorial.jpa.repositories;
+
+import edu.unbosque.JPATutorial.jpa.entities.Author;
+import edu.unbosque.JPATutorial.jpa.entities.Library;
+
+import javax.persistence.EntityManager;
+import java.util.List;
+import java.util.Optional;
+
+public class AuthorRepositoryImpl implements AuthorRepository {
+
+    private EntityManager entityManager;
+
+    public AuthorRepositoryImpl(EntityManager entityManager) {
+        this.entityManager = entityManager;
+    }
+
+    public Optional<Author> findById(Integer id) {
+        Author author = entityManager.find(Author.class, id);
+        return author != null ? Optional.of(author) : Optional.empty();
+    }
+
+    public List<Author> findAll() {
+        return entityManager.createQuery("from Author").getResultList();
+    }
+
+    public Optional<Author> findByName(String name) {
+        Author author = entityManager.createNamedQuery("Author.findByName", Author.class)
+                .setParameter("name", name)
+                .getSingleResult();
+        return author != null ? Optional.of(author) : Optional.empty();
+    }
+
+    public Optional<Author> save(Author author) {
+        try {
+            entityManager.getTransaction().begin();
+            entityManager.persist(author);
+            entityManager.getTransaction().commit();
+            return Optional.of(author);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return Optional.empty();
+    }
+
+    @Override
+    public void deleteById(Integer id) {
+        Author author = entityManager.find(Author.class, id);
+        if (author != null) {
+            try {
+
+                entityManager.getTransaction().begin();
+
+                author.getBooks().forEach(book -> {
+                    entityManager.remove(book);
+                });
+
+                entityManager.remove(author);
+                entityManager.getTransaction().commit();
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+}
